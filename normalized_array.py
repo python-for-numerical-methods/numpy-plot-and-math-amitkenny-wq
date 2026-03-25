ca97a1267e76f5ef1acdeb71bd805eea30e32868
Amit Kenny <amit.kenny@gmail.com>
2026-03-25 08:06:57 +0200

    Add normalization function for input arrays
    לבדוק את מערכת הבדיקה האוטומטית

--- normalized_array.py
+++ normalized_array.py
@@ -1,24 +1,13 @@
 import numpy as np
 
-def normalized_array(data):
-    """
-    מנרמלת מערך נתונים לטווח של [0, 1] לפי שיטת Min-Max Scaling.
-    
-    הנוסחה לביצוע:
-    x_norm = (x - min) / (max - min)
-    
-    פרמטרים:
-    data (list or np.array): מערך של מספרים.
-    
-    מחזירה:
-    np.array: מערך מנורמל. אם כל הערכים במערך זהים, יש להחזיר מערך של אפסים.
-    """
-    # המרת הקלט ל-numpy array לצורך חישובים וקטוריים
-    data = np.array(data)
-    
-    # --- כיתבו את הקוד שלכם כאן ---
-    pass
-    # חשוב לזכור להחליף את pass ב- return
+def normalized_array(input_array):
+  data = input_array.copy()
+  data = np.array(data)
+  if np.min(data) == np.max(data):
+    return np.zeros(data.shape)
+  else:
+    data = (data - np.min(data)) / (np.max(data) - np.min(data))
+  return data
 
 if __name__ == "__main__":
     # כאן הסטודנטים יכולים להריץ בדיקה עצמית מהירה
